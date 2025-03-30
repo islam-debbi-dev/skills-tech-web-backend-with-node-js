34b3b2752fd58ef78983abe900acb063ba79a5cf
detsabirde <130311972+detsabirde@users.noreply.github.com>
2025-03-30 15:07:42 +0100

    - initialize connection to database and fix the server not working, see comment in index.js.

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express');
-const app = express();
 const auth = require('./routes/auth');
+const dotenv = require('dotenv');
+const connectDB = require('./middlewares/conn-db');
 
-app.use("/auth",auth);
+dotenv.config();
 
+const app = express();
 const port = 3000;
+
+// hna srali mochkel brk ,ga3ed ytawal w huwa yverifier connection w mb3d 
+// ygouli error chuflna lmochkel ya islem
+// ghirha kolch raw ymchiw riglo
+// seha 3idk 3yniya
+// connectDB();
+
+app.use(express.json());
+app.use("/auth",auth);
 
 app.listen(port, () => {
   console.log(` localhost:${port}/`);
