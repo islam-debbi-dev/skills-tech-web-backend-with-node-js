abf4113daf26c87ca40b89b84ead7ffb8216e4e2
detsabirde <130311972+detsabirde@users.noreply.github.com>
2025-04-01 16:25:53 +0100

    - add a checkRole function - add users route - add error handler at both server side and client side

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const auth = require('./routes/auth');
+const users = require('./routes/user');
 const connectDb = require('./middleware/connactDb');
 const port = process.env.PORT || 3000;
 const app = express();
@@ -13,7 +14,18 @@ connectDb();
 
 // routes
 app.use("/api/auth",auth);
+app.use("/api/user",users);
 
+// error handler when page not found
+app.use((req, res) => {
+  res.status(404).render("404", { title: "Page not found." });
+});
+
+// error handler at server side 
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).render("error", { title: "An error on the server.", error: err });
+});
 
 app.listen(port, () => {
   console.log(` localhost:${port}/`);
